7909e87dc7938d41c9f8b765af8a4cef4313bc49
IgorBonfim <igormbonfim@hotmail.com>
2023-05-18 20:00:41 -0300

    Initial commit

---
+++ src/main/java/com/igorbonfim/vuttrapi/VuttrapiApplication.java
@@ -0,0 +1,13 @@
+package com.igorbonfim.vuttrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuttrapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuttrapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorbonfim/vuttrapi/VuttrapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorbonfim.vuttrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuttrapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
